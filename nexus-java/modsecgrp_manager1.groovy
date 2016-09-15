4b9d1c8b4aa2efff8cd3c85b1809e0d960db13eb
EC2 Default User <ec2-user@ip-172-31-17-253.us-west-2.compute.internal>
2016-09-15 13:07:51 +0000

    Add groovy code to open ports between swarm manager1 and consul

---
+++ nexus-java/modsecgrp_manager1.groovy
@@ -0,0 +1,92 @@
+import groovy.json.JsonSlurper
+
+/************
+usage:
+   vi modsecgrp.groovy
+   ~/.sdkman/candidates/groovy/2.4.6/bin/groovyc modsecgrp.groovy
+   java -cp .:./groovy-2.4.7.jar:./groovy-json-2.4.7.jar modsecgrp
+************/
+
+def getSecurityGroupID(keyvalue) {
+ def awscmd = 'aws ec2 describe-security-groups --region us-west-2'
+ def awscmdtext = awscmd.execute().text
+ def slurper = new JsonSlurper()
+ def result = slurper.parseText(awscmdtext)
+ def keynames = result.SecurityGroups.GroupName
+ def keyclosure = { it =~ keyvalue }
+ def count = keynames.grep(keyclosure).size()
+ if (count == 0) {
+  println "GroupName '$keyvalue' not found. exiting with rc=-1"
+  System.exit(-1)
+ }
+ if (count > 1) {
+  println "GroupName '$keyvalue' too common. exiting with rc=-1"
+  println keynames.grep(keyclosure)
+  System.exit(-1)
+ }
+ def ixkeyname = keynames.findIndexOf(keyclosure)
+ result.SecurityGroups.GroupId[ixkeyname]
+}
+
+def getInstanceIPAddresses(keyvalue) {
+ def awscmd = 'aws ec2 describe-instances --region us-west-2' +
+              ' --filters Name=instance-state-name,Values=running'
+ def awscmdtext = awscmd.execute().text
+ def returnResult = [:]
+
+ def slurper = new JsonSlurper()
+ def result = slurper.parseText(awscmdtext)
+ result.Reservations.Instances.each { i ->
+  def found = i.Tags[0].grep {
+   (it.Key == 'Name') && (it.Value == keyvalue)
+  }  
+  if (found.size() > 0) {
+   returnResult.privateIpAddress = i[0].PrivateIpAddress
+   returnResult.publicIpAddress =  i[0].PublicIpAddress
+  }
+ }
+ returnResult
+}
+
+def authorizeSecurityGroupIngress(comment, groupID, cidr, port) {
+ def proc
+ def sb
+
+ def cmd = 
+       "aws ec2 authorize-security-group-ingress " +
+       " --group-id $groupID" +
+       " --cidr $cidr/32" +
+       " --port $port " +
+       " --protocol tcp " +
+       " --region us-west-2 " 
+ proc = cmd.execute()
+ sb = new StringBuffer()
+ proc.consumeProcessErrorStream(sb)
+ println "comment: \n$comment"
+ println "generated AWS CLI command: \n$cmd"
+ println "stdout: ${proc.text}"
+ println "stderr: ${sb.toString()}"
+ println ""
+}
+
+/**
+* Update AWS security groups
+**/
+
+def manager1IPs                      = getInstanceIPAddresses('swarm2')
+def manager1SecurityGroupID          = getSecurityGroupID('swarm2-SecurityGroup')
+
+def consulIPs                        = getInstanceIPAddresses('swarm1')
+def consulSecurityGroupID            = getSecurityGroupID('swarm1-SecurityGroup')
+
+authorizeSecurityGroupIngress(
+ 'allow manager1 to receive from consul on 4000', 
+ manager1SecurityGroupID, 
+ consulIPs.publicIpAddress, 
+ '4000')
+
+authorizeSecurityGroupIngress(
+ 'allow consul to receive from manager1 on 4000', 
+ consulSecurityGroupID, 
+ manager1IPs.publicIpAddress, 
+ '4000')
