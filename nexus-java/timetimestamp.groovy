bce88647d35494accf7500d1c4584da2d23f8726
EC2 Default User <ec2-user@ip-172-31-26-160.us-west-2.compute.internal>
2016-10-11 23:51:08 +0000

    Wrap code into methods

--- nexus-java/timetimestamp.groovy
+++ nexus-java/timetimestamp.groovy
@@ -11,55 +11,35 @@ import groovy.json.*
 import java.sql.*
 import java.text.*
 
-def timetimestamp(attimestamp, time) {
-  SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-  sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-  def xtimestamp = attimestamp //result2AsJson.hits.hits[0]._source."@timestamp"
-  def xtimestamp2 = xtimestamp[0..18]+"Z"
-  def xtimestamp3 = sdf.parse(xtimestamp2)
-  //println xtimestamp3
-  def xtime = time //result2AsJson.hits.hits[0]._source.time
-  def xtime2 = xtime[0..18]+"Z"
-  def xtime3 = sdf.parse(xtime2)
-  //println xtime3
-  def totalSeconds
-  use(groovy.time.TimeCategory) {
-    def duration = xtimestamp3 - xtime3
-    //println "Hours: ${duration.hours}"
-    //println "Minutes: ${duration.minutes}"
-    //println "Seconds: ${duration.seconds}"
-    totalSeconds = duration.minutes*60 + duration.seconds
-  }
-  totalSeconds
-}
+ def computeAverage() {
+  def body4 = "{\"size\": 0, \"aggs\": { \"avg_grade\": { \"avg\": { \"field\": \"seconds\"}}}}"
+
+  String url4 = "http://52.37.229.115:9200/seconds-2016.10.08/_search"
+  println "url $url4"
+  def myprocess4 = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${body4}' $url4"].execute()
+  myprocess4.waitFor()
+  String myprocess4AsText = myprocess4.text
+
+  def result4AsJson = new JsonSlurper().parseText(myprocess4AsText)
+  result4AsJson.aggregations.avg_grade.value
+ }
+
 /*
-  println "--start of test 1--"
-  def body2 = '{"url":"http://pzsvc-hello.int.geointservices.io","method":"GET","contractUrl":"http://pzsvc-hello.int.geointservices.io/","resourceMetadata":{"name":"Hello World Test","description":"Hello world test","classType":{"classification":"UNCLASSIFIED"}}}'
-
-  def myprocess2 = [ 'bash', '-c', "curl -v -k -X GET http://52.37.229.115:9200/logstash-2016.10.08" ].execute()
-  myprocess2.waitFor()
-  String myprocess2AsText =  myprocess2.text
-  println myprocess2AsText
-
-  def result2AsJson = new JsonSlurper().parseText(myprocess2AsText)
-
-  println result2AsJson
-  println "--end of test 1--"
-curl http://52.37.229.115:9200#/logstash-2016.10.08/_search?from=23 -d '{"size":2,"query":{"match_all":{}}}'
+ Read from logstash-2016.10.08 and
+ write to seconds-2016.10.08
 */
-
-  println "--start of test 2--"
+ def readThenWrite(logstashname) { //ixname logstash-2016.10.08
   def SIZE = 10
   def body2 = '{"query":{"match_all":{}}}'
 
   def fetchMoreData = true
   for (def from = 0; fetchMoreData; from += SIZE) {
-   String url = "http://52.37.229.115:9200/logstash-2016.10.08/_search?from=$from\\&size=$SIZE"
+   String url = "http://52.37.229.115:9200/$logstashname/_search?from=$from\\&size=$SIZE"
    println "url $url"
    def myprocess2 = [ 'bash', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${body2}' $url"].execute()
    myprocess2.waitFor()
    String myprocess2AsText = myprocess2.text
-   //println "xxx\n$myprocess2AsText\nyyy"
+
    def result2AsJson = new JsonSlurper().parseText(myprocess2AsText)
    def id = result2AsJson.hits.hits._id
    def attimestamp = result2AsJson.hits.hits._source."@timestamp"
@@ -68,26 +48,43 @@ curl http://52.37.229.115:9200#/logstash-2016.10.08/_search?from=23 -d '{"size":
    for (def i = 0; i < id.size(); i++) {
     def t = timetimestamp(attimestamp[i], time[i])
     println "${id[i]} $t"
-    //def body = "{\"id\":\"${id[i]}\", \"time\":${time[i][0..18]+"Z"}, \"seconds\": $t}"
     def body = "{\"time\":${time[i][0..18]+"Z"}, \"seconds\": $t}"
     isrt(id[i], time[i][0..18]+"Z", t)
    }
    fetchMoreData = (id.size() == SIZE)
   }
-  println "--end of test 2--"
-/*
- curl -XPUT http://52.37.229.115:9200/seconds-2016.10.08/mytype/AVelsxTHYmEacSoOqezI -d '{"time":"2016-10-08T19:08:58Z", "seconds": 20}'
-*/
+ }
+
  def isrt(id, timexx, secs) {
    println id
    String url2 = "http://52.37.229.115:9200/seconds-2016.10.08/mytype/$id "
    println "url2 $url2"
 
-   //def myprocessa = [ 'bash', '-c', "curl -v -k -X POST  -d '{\"seconds\":44, \"lime\":33, \"motali\":\"2016-10-08T19:08:58Z\"}' $url2"]
    def myprocessa = [ 'bash', '-c', "curl -v -k -X POST  -d '{\"seconds\":$secs, \"lime\":33, \"motali\":\"$timexx\"}' $url2"]
    def myprocess3 = myprocessa.execute()
    myprocess3.waitFor()
    String myprocess3AsText = myprocess3.text
    println myprocess3AsText
   }
- 
+
+ def timetimestamp(attimestamp, time) {
+  SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+  sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+  def xtimestamp = attimestamp 
+  def xtimestamp2 = xtimestamp[0..18]+"Z"
+  def xtimestamp3 = sdf.parse(xtimestamp2)
+
+  def xtime = time 
+  def xtime2 = xtime[0..18]+"Z"
+  def xtime3 = sdf.parse(xtime2)
+
+  def totalSeconds
+  use(groovy.time.TimeCategory) {
+    def duration = xtimestamp3 - xtime3
+    totalSeconds = duration.minutes*60 + duration.seconds
+  }
+  totalSeconds
+ }
+
+ readThenWrite("logstash-2016.10.08")
+ println computeAverage() 
